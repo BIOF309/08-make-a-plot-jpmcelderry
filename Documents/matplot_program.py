db6e50cb2b8c04e21c53d96e900e05d7bc4e75f1
jpmcelderry <john.p.mcelderry@gmail.com>
2017-11-27 16:40:38 -0500

    first commit

---
+++ Documents/matplot_program.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016],[19.12,20.9,22.79,23.52,22.75,24.08,27.01,27.57,28.11,27.44,25.41,24.62])
+plt.ylabel('Revenue in Billions of Dollars')
+plt.xlabel('Year')
+plt.title("McDonald's Revenue by Year")
+plt.show()
