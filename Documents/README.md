dc12b5b153d6e63f07f468ea3310972e33ce0e6c
jpmcelderry <john.p.mcelderry@gmail.com>
2017-11-27 15:49:05 -0500

    first commit

---
+++ Documents/README.md
@@ -0,0 +1,1 @@
+# cubed program for the cubed assignment. Program cubes a number, nothing more nothing less
---
+++ Documents/cubed.py
@@ -0,0 +1,3 @@
+print("Enter your number below to cube it:")
+orig_number=float(input())
+print(orig_number**3)
